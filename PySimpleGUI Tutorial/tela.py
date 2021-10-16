4eb16fb876e87e6e0a69c1c7a2c63873fbc18523
Leonardo César <leocesar1977@Gmail.com>
2021-10-16 11:44:40 -0300

    PyguiTutorial

---
+++ PySimpleGUI Tutorial/tela.py
@@ -0,0 +1,10 @@
+import PySimpleGUI as sg
+
+class TelaPython():
+    def __init__(self):
+        # Layout
+        layout = [
+            
+        ]
+        # Janela
+        # Extrair os dados da tela
